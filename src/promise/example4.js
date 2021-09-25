6da24f0e7714ceb0951fbe5ccededaccbf9fd789
“Samir <serodas@gmail.com>
2021-09-24 23:13:36 -0500

    Manejo de excepciones en sync await

--- src/promise/example4.js
+++ src/promise/example4.js
@@ -3,24 +3,25 @@ const API = 'https://rickandmortyapi.com/api/character/';
 const fetchData = API =>  {
     return new Promise((resolve,reject) => {
         $.get(API,function(response){
-            if(true){
-                resolve(response);
-            }else{
-                reject(new Error('An Error ocurred!',API));
-            }
-        });
+            resolve(response);
+        })
+        .fail(error => reject( new Error(error)));
     })
 }
 
 const exampleAsync = async(API) => {
-    const data = await fetchData(API);
-    console.log(data.info.count);
+    try{
+        const data = await fetchData(API);
+        console.log(data.info.count);
 
-    const character = await fetchData((data.results[0].origin.url));
-    console.log(character.dimension);
+        const character = await fetchData((data.results[0].origin.url));
+        console.log(character.dimension);
 
-    const resident = await fetchData(character.residents[0]);
-    console.log(resident.name);
+        const resident = await fetchData(character.residents[0]);
+        console.log(resident.name);
+    }catch(error){
+        console.log(error);
+    }
 }
 
 console.log('Before!');
